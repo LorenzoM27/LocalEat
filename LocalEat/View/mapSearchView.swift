0590eaaf0aa7468195c3965d369ce886c10e4060
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-02-13 17:01:58 +0100

    new Map location Search

---
+++ LocalEat/View/mapSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  mapSearchView.swift
+//  LocalEat
+//
+//  Created by Lorenzo Menino on 13/02/2024.
+//
+
+import SwiftUI
+
+struct mapSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapSearchView()
+}
