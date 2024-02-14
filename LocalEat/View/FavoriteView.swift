93099c99fe28954c5532bcb40116bd9a1cbb57c5
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-02-14 10:46:42 +0100

    some files error

--- LocalEat/View/FavoriteView.swift
+++ LocalEat/View/FavoriteView.swift
@@ -2,14 +2,14 @@
 //  FavoriteView.swift
 //  LocalEat
 //
-//  Created by Lorenzo Menino on 12/02/2024.
+//  Created by Lorenzo Menino on 14/02/2024.
 //
 
 import SwiftUI
 
 struct FavoriteView: View {
     var body: some View {
-        Text("Favorite")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
