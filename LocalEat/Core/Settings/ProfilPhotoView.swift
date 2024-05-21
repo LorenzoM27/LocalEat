1c5da57f107db18d01afff357af9887b76fef54b
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-05-21 10:42:18 +0200

    different modifications

---
+++ LocalEat/Core/Settings/ProfilPhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilPhotoView.swift
+//  LocalEat
+//
+//  Created by Lorenzo Menino on 21/05/2024.
+//
+
+import SwiftUI
+
+struct ProfilPhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilPhotoView()
+}
